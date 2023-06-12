e137536b24c296eac568ad53e2d1683b06bd7007
kaki458 <kicinskikacper@gmail.com>
2023-06-12 23:30:36 +0200

    timer0 interrupts

--- 3_leds_control.c
+++ 3_leds_control.c
@@ -11,7 +11,6 @@
 #define BUTTON2 PB1
 #define BUTTON3 PB2
 
-volatile uint8_t BUTTON_CLICKED = 0;
 volatile uint8_t LED_STATE = 0;
 
 void setUpIO() {
@@ -20,10 +19,11 @@ void setUpIO() {
 	PORTB |= (1 << BUTTON1) | (1 << BUTTON2) | (1 << BUTTON3);
 }
 
-void setUpTimer() {
+void setUpTimers() {
 	TCCR1B |=  (1 << WGM12) | (1 << CS12) | (1 << CS10);
-  // ~20ms
-	OCR1A = 312;
+	OCR1A = 78; // ~5ms
+
+  TCCR0B |= (1 << CS02) | (1 << CS00); 
 }
 
 void setUpInterrupts() {
@@ -35,67 +35,134 @@ bool isPressed(uint8_t pinb, int button) {
 	return !(pinb & (1 << button));
 }
 
-bool isNotPressed(uint8_t pinb, int button) {
-	return (pinb & (1 << button));
+bool anyPressed() {
+  return isPressed(PINB, BUTTON1) || isPressed(PINB, BUTTON2) || isPressed(PINB, BUTTON3);
 }
 
 void toggle(int led) {
   PORTD ^= (1 << led);
 }
 
+void enableTimer0Interrupts() {
+  TIMSK0 |= (1 << TOIE0);
+}
 
-void controlOutputs(uint8_t pinb) {
+void disableTimer0Interrupts() {
+  TIMSK0 &= ~(1 << TOIE0);
+}
 
-  if ( isPressed(pinb, BUTTON1) && isNotPressed(pinb, BUTTON2) && isNotPressed(pinb, BUTTON3) ) {
+void reset() {
+  if (LED_STATE != 0) {
+    disableTimer0Interrupts();
+    LED_STATE = 0;
+    PORTD = 0;
+  }
+}
+
+void changeState(uint8_t pinb) {
+
+  if ( isPressed(pinb, BUTTON1) && !isPressed(pinb, BUTTON2) && !isPressed(pinb, BUTTON3) ) {
+  	reset();
+    toggle(LED1);
   
-  toggle(LED1);
+  } else if ( !isPressed(pinb, BUTTON1) && isPressed(pinb, BUTTON2) && !isPressed(pinb, BUTTON3) ) {
+    reset();
+	  toggle(LED2);
   
-  } else if ( isNotPressed(pinb, BUTTON1) && isPressed(pinb, BUTTON2) && isNotPressed(pinb, BUTTON3) ) {
+  } else if ( !isPressed(pinb, BUTTON1) && !isPressed(pinb, BUTTON2) && isPressed(pinb, BUTTON3) ) {
+  	reset(); 
+    toggle(LED3);
   
-	toggle(LED2);
+  } else if ( isPressed(pinb, BUTTON1) && isPressed(pinb, BUTTON2) && !isPressed(pinb, BUTTON3) ) {
+    if (LED_STATE != 1) {
+      PORTD = 0;
+	    LED_STATE = 1;
+      enableTimer0Interrupts();
+    }
   
-  } else if ( isNotPressed(pinb, BUTTON1) && isNotPressed(pinb, BUTTON2) && isPressed(pinb, BUTTON3) ) {
-  
-	toggle(LED3);
-  
-  } else if ( isPressed(pinb, BUTTON1) && isPressed(pinb, BUTTON2) && isNotPressed(pinb, BUTTON3) ) {
-	
-	toggle(LED3);
-  
+  } else if ( isPressed(pinb, BUTTON1) && !isPressed(pinb, BUTTON2) && isPressed(pinb, BUTTON3) ) {
+	  if (LED_STATE != 2) {
+      PORTD = 0;
+      LED_STATE = 2; 
+      enableTimer0Interrupts();
+    }
+  } 
+}
+
+void toggleAllLeds() {
+  PORTD ^= (1 << LED1) | (1 << LED2) | (1 << LED3);
+}
+
+void blinkLedsOneByOne() {
+  if (PORTD == (1 << LED1)) {
+    PORTD = 0;
+    PORTD |= (1 << LED2);
+  } else if (PORTD == (1 << LED2)) {
+    PORTD = 0;
+    PORTD |= (1 << LED3);
+  } else {
+    PORTD = 0;
+    PORTD |= (1 << LED1);
   }
 }
 
 int main(void) {
 	
 	setUpIO();
-	setUpTimer();
+	setUpTimers();
 	setUpInterrupts();
 	
 	while (1) {
 
-	}
+  }
 }
 
 ISR(TIMER1_COMPA_vect) {
 	
 	static uint8_t counter = 0;
-	
+  static bool buttonClicked = false;
+	static bool buttonDebounced = false;
+  static bool actionPerformed = false;
 	uint8_t pinb = PINB;
 
-	if (!BUTTON_CLICKED && ( isPressed(pinb, BUTTON1) || isPressed(pinb, BUTTON2) || isPressed(pinb, BUTTON3) )) {
-		BUTTON_CLICKED = 1;
-		counter = 20; // ~400ms
-		controlOutputs(pinb);
-	} 
+	if (!buttonClicked && anyPressed()) {
+    buttonClicked = true;
+    buttonDebounced = false;
+    actionPerformed = false;
+		counter = 80; // ~400ms
+	}
 		
-	if (BUTTON_CLICKED == 1) {
+	if (buttonClicked) {
 		if (counter > 0) {
 			counter--;
-		} else {
-			BUTTON_CLICKED = 0;
+      if (counter == 76) {
+        buttonDebounced = true;
+      }
+    } else {
+			buttonClicked = false;
 		}
 	}
+
+  if (!actionPerformed && buttonClicked && buttonDebounced) {
+    pinb = PINB;
+    changeState(pinb);
+    actionPerformed = true;
+  } 
 }
 
+ISR(TIMER0_OVF_vect) {
+    static uint8_t counts = 0;
+    if (counts == 30) { // ~500ms
+      if (LED_STATE == 1) {
+        toggleAllLeds();
+      } else if (LED_STATE == 2) {
+        blinkLedsOneByOne();
+      }
+      counts = 0;
+    }
+    else {
+      counts++;
+    }
+}
 
 
